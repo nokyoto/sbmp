a86e611cc27a93180cd5945bbe60b08c3ab604f1
kyokyo <nktoooo@proton.me>
2026-03-23 10:35:50 -0400

    :P
    :P

--- colors2.c
+++ colors2.c
@@ -28,7 +28,6 @@ uint16_t	hdr_ysize	=		3;			// height
 // now the data
 // of which is 000110010101001111011011011111111110
 // but i could not in good faith use a c standard data type for it
-// :P
 
 // and the rest was history
 int main(){return 1;} // don't mind me just forcing g++ to compile
